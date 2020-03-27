11c3c5b077bf109f5a9d403465b4d6cb05b5b874
wickker <choydianchun@gmail.com>
2020-03-27 17:01:38 +0800

    completed playersetup function

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //Global variables
 let gamePlayArr = [];
 let boardSize = 5;
+let gameProgressState = false;
 
 //DOM get variables 
 let gameBoardDiv = document.getElementById("game-board");
 
+//Initialize empty game board 
 function initGameBoard() {
   for (let x=0; x < boardSize; x++) {
     gamePlayArr.push([]);
     for (let y=0; y < boardSize; y++) {
-      gamePlayArr[x].push("");
+      gamePlayArr[x].push("dead");
       let newCell = document.createElement("div");
       newCell.className = "cell";
       newCell.classList.add("dead");
@@ -22,8 +24,22 @@ function initGameBoard() {
   }
 }
 
-function playerSetUp() {
-  console.log("color change!");
+//Lets player set up initial "dead" or "alive" state of game board
+function playerSetUp(event) {
+  //Only functions when game is not in progress 
+  if (gameProgressState === false) {
+    //Change cell state to "alive" in DOM
+    event.target.classList.remove("dead");
+    event.target.classList.add("alive");
+    //Update cell state in gamePlayArr
+    let clickedCellIndex = event.target.id.split("-");
+    let xCo = clickedCellIndex[0];
+    let yCo = clickedCellIndex[1];
+    gamePlayArr[xCo][yCo] = "alive";
+    console.log(gamePlayArr);
+  }
 }
+
+
 
 initGameBoard();
